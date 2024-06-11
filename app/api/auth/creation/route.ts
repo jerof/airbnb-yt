9a6d9758ffa73cf8ca13f005403a85dee7affc77
Nikhil Samuel <nikhilsamuel.89@gmail.com>
2024-06-11 21:46:41 +0200

    done cachingagain 2

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -10,7 +10,7 @@ export async function GET() {
   const user = await getUser();
 
   if (!user || user === null || !user.id) {
-    throw new Error("Smoething went wrong, i am srorry....");
+    throw new Error("Something went wrong, i am sorry....");
   }
 
   let dbUser = await prisma.user.findUnique({
@@ -32,5 +32,5 @@ export async function GET() {
     });
   }
 
-  return NextResponse.redirect("https://airbnb-yt-peach.vercel.app");
+  return NextResponse.redirect("https://airbnb-yt-sable.vercel.app");
 }
